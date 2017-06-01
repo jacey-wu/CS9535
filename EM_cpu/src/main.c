ed82091ce44be64a5a0e6d180537bed14ed74c75
Jacey Wu <jiaxiao.wu@mail.utoronto.ca>
2017-06-01 02:02:50 -0400

    Add some doc on EM_CPU main

--- EM_cpu/src/main.c
+++ EM_cpu/src/main.c
@@ -1,12 +1,34 @@
+/**
+    CS9535 Expactation-maximization (EM) algorithm (CPU impl)
+    main.c
+    Purpose: The main file of the CPU implementation of EM algorithm. Parse cmd
+    arguments and complete I/O operations.
+
+    @author Jacey Wu
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <math.h>
 #include <time.h>
 
 #include "EM_cpu.h"
 
+#define DEFAULT_DIM 3
 
+//extern char *optarg;
+
+/**
+ * Print mean vector MU and covariance matrix SIGMA for each Gaussian model
+ * stored in Gaussian parameter array.
+ *
+ * @param params: the Gaussian parameter array
+ * @param num_models: the number of Gaussian models included in GMM
+ * @param dim: the dimension of input feature
+ * @return
+ */
 void print_params(GaussianParam *params, int num_models, int dim)
 {
 	printf("-- The Gaussian parameters estimated --\n");
@@ -24,14 +46,79 @@ void print_params(GaussianParam *params, int num_models, int dim)
 	}
 }
 
+
+/**
+ * Print help message.
+ */
+void print_help()
+{
+	printf("\nUsage  : EM_CPU [options] <inputvalue>\n");
+	printf("Multiple arguments can be provided separated by wightspace, e.g.\n");
+	printf("EM_cpu -f input_file.csv -n 5000 -g 4 -i 1000\n\n");
+	printf("Supported options:\n");
+	printf("  -f csv_file  Option to add input file in csv format\n");
+	printf("  -n size      Option to specify the number of input samples in given csv file\n");
+	printf("  -g num_gaus  Option to specify the number of Gaussian models\n");
+	printf("  -i max_iter  Option to specify the max number of iterations of EM algo\n");
+	printf("  -h           Option to show this message\n\n");
+}
+
+
+/**
+ * Main function to launch EM experiments.
+ *
+ * @param argc: the number of cmd arguments
+ * @param argv: the cmd arguments as strings
+ * @return: error code
+ */
 int main(int argc, char *argv[])
 {
-	const char *csv_file = "src/data/static_G3_N100000.csv";
-	const int dim = 3;
-	const int size = 50000;
-	const int num_gaus = 3;
-	const int num_iter = 5000;
-	double exit_threshold = 1e-20;
+	/* Default values of cmd arguments */
+	const double exit_threshold = 1e-20;
+	const int dim = DEFAULT_DIM;
+
+	char *csv_file = "../src/data/static_G3_N1000.csv";
+	int size = 1000;
+	int num_gaus = 3;
+	int num_iter = 500;
+
+	// Process cmd arguments
+	if (argc < 2)
+	{
+		print_help();
+		printf("None options are received. Process with default args\n");
+	}
+	else
+	{
+		int opt = 0;
+		const char *optString = "f:n:g:i:h?";
+
+		opt = getopt( argc, argv, optString);
+		while (opt != -1) {
+			switch (opt) {
+			case 'f':	// input file
+				csv_file = optarg;
+				printf("File name: %s\n", csv_file);
+				break;
+			case 'n':	// num of samples
+				size = atoi(optarg);
+				break;
+			case 'g':	// num of Gaussian models
+				num_gaus = atoi(optarg);
+				break;
+			case 'i':	// num of max iterations
+				num_iter = atoi(optarg);
+				break;
+			case 'h':	// help message
+			case '?':
+				print_help();
+				break;
+			default:	// should never reach here
+				break;
+			}
+			opt = getopt(argc, argv, optString);
+		}
+	}
 
 	// Read samples from csv file
 	char buffer[1024];
@@ -43,19 +130,19 @@ int main(int argc, char *argv[])
 	FILE *fstream = fopen(csv_file, "r");
 	if (fstream == NULL)
 	{
-		printf("\n file opening failed ");
+		printf("[ERROR] Failed to open file %s\n", csv_file);
 		return -1;
 	}
 
 	i = 0;
-	while (fgets(buffer, sizeof(buffer), fstream) != NULL)
+	while (i < size && fgets(buffer, sizeof(buffer), fstream) != NULL)
 	{
 		j = 0;
 		sample = (double *)malloc(sizeof(double) * dim);
 		token = strtok(buffer, ",");
 		while (token != NULL)
 		{
-			// printf("read in %d sample token: %s\n", i, token);
+			//printf("read in %d sample token: %s\n", i, token);
 			sample[j] = atof(token);
 			token = strtok(NULL, ",");
 			j++;
